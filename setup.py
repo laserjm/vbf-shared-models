18e267dfe2ca4ebe6c4eaf3061ecb81fd9546bea
laserjm <peter_planlos@web.de>
2023-04-25 18:57:50 +0200

    added pydantic to requirements in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='VbfSharedModels',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['pydantic'],
     author='Jan-Michael Laser',
     author_email='jmlaser@web.com',
     description='A package for sharing models',
