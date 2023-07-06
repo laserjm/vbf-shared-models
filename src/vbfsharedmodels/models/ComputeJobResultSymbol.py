838c995d4935f5b575df8fc3fe2d4c1f51d34d35
laserjm <peter_planlos@web.de>
2023-07-06 14:35:34 +0200

    added schema to model

--- src/vbfsharedmodels/models/ComputeJobResultSymbol.py
+++ src/vbfsharedmodels/models/ComputeJobResultSymbol.py
@@ -8,3 +8,21 @@ class ComputeJobResultSymbol(BaseModel):
     symbol: str
     job_state: JobState
     data: Optional[object]
+
+    class Config:
+        schema_extra = {
+            "symbol": "AAPL",
+            "job_state": {
+                "name": "finished",
+                "message": ""
+            },
+            "data": {
+                "gates_passed": 8,
+                "weighted_score": 4.0,
+                "symbol": "ABC",
+                "isin": "DE019282HFU",
+                "company_name": "AB Co.",
+                "price": 123.45,
+                "market_cap": 100000000000
+            }
+        }
